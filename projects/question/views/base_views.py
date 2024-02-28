f4d79e1e1731b68bf66a08a168fdeac2b1a34df1
Uheeking <wert1847@naver.com>
2024-02-28 18:38:44 +0900

    🎨 ADD : static file added

--- projects/question/views/base_views.py
+++ projects/question/views/base_views.py
@@ -5,6 +5,7 @@ from django.db.models import Q
 from ..models import Question
 
 def index(request):
+    Uheeking = 'Uheeking'
     # page = request.GET.get('page', '1')  # 페이지
-    # context = {'question_list': page_obj, 'custom_range':custom_range, 'max_index': max_index, 'page': page, 'kw': kw}
-    return HttpResponse("안녕하세요 question 오신것을 환영합니다.")
+    context = { 'Uheeking':Uheeking}
+    return render(request, 'question/profile.html', context)
