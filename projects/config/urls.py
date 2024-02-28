d8f33801fe875d411a9c49cdd27d7d7085ffa7d4
Uheeking <wert1847@naver.com>
2024-02-28 20:47:29 +0900

    🎨 ADD : 로그인 화면 구현

--- projects/config/urls.py
+++ projects/config/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-# from pybo.views import base_views
+from question.views import base_views
 
 urlpatterns = [
-    # path('', base_views.index, name='index'),  
+    path('', base_views.index, name='index'),  # '/' 에 해당되는 path
     path('admin/', admin.site.urls),
     path('question/', include('question.urls')),
-    # path('common/', include('common.urls')),
+    path('common/', include('common.urls')),
 ]
