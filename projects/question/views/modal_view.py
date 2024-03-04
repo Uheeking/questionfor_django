ef0c5b35460146ae9269e06c9fd0fce36963cef1
Uheeking <wert1847@naver.com>
2024-03-04 14:08:48 +0900

    🎨 MODIFY : 유저 등록

--- projects/question/views/modal_view.py
+++ projects/question/views/modal_view.py
@@ -1,4 +1,5 @@
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from question.forms import QuestionForm
 from django.views.decorators.csrf import csrf_exempt
@@ -6,11 +7,13 @@ from django.views.decorators.csrf import csrf_exempt
 from ..models import Question
 
 @csrf_exempt
+@login_required(login_url='common:login')
 def createQuestion(request):
     if request.method == "POST":
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user
             question.create_date = timezone.now()
             question.save()
             return redirect('question:index')
